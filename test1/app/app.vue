549afb83de5da76fdd96ea42cb0a248d770c7f46
satone <kyotosato1107@gmail.com>
2025-07-30 21:46:47 +0900

    学習: Vueの基本を学んだ

--- test1/app/app.vue
+++ test1/app/app.vue
@@ -1,6 +1,17 @@
+<script setup lang="ts">
+  const gomugomu = Math.round(Math.random() * 100);
+  const marchenGomuGomu = gomugomu * 10;
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    aiueo
-  </div>
+  <p>
+    {{ gomugomu }} ゴムゴムは {{ marchenGomuGomu }} メルヘンゴムゴム
+  </p>
 </template>
+
+<style>
+  p{
+    color: red;
+  }
+
+</style>
