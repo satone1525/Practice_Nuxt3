03abb04448a9997a20434850cded7e968100af3c
satone <kyotosato1107@gmail.com>
2025-07-30 23:00:21 +0900

    学習&新規: Emits

--- test1/app/app.vue
+++ test1/app/app.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
   // import Nami from '../components/Nami.vue';
-  import OneCharacter from '../components/OneCharacter.vue';
+  import OneCharacter from '../components/OneCharacter2.vue';
   // import Luffy from '../components/monkey/Luffy.vue';
 
   //キャラクターリストデータ
   const characterListInt = new Map<number, Character>();
-  characterListInt.set(1,{id:1, name: "ルフィ", bounty: 100000000});
-  characterListInt.set(2,{id:2, name: "ゾロ", bounty: 60000000});
+  characterListInt.set(1,{id: 1, name: "ルフィ", bounty: 100000000});
+  characterListInt.set(2,{id: 2, name: "ゾロ", bounty: 60000000});
   const characterList = ref(characterListInt);
 
   //懸賞金合計の算出プロパティ
@@ -18,8 +18,17 @@
       }
       return total;
     }
-  )
+  );
 
+  const onIncrementBounty = (id: number) : void => {
+    //オブジェクトを取得
+    const character = characterList.value.get(id);
+    //オブジェクトが存在するなら
+    if(character != undefined){
+      //懸賞金アップ
+      character.bounty += 1000;
+    }
+  }
 
   //キャラクターインターフェース
   interface Character{
@@ -36,8 +45,10 @@
     <OneCharacter
       v-for="[id, character] in characterList"
       :key = "id"
-      :name = "character.name"
-      :bounty="character.bounty"
+      :id = "id"
+      :name ="character.name"
+      :bounty = "character.bounty"
+      v-on:incrementBounty = "onIncrementBounty"
     />
   </section>
 </template>
---
+++ test1/components/OneCharacter2.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+// 1. 子でpropsの準備
+//親から受け取るプロパティはdefineProps()で型宣言する
+//(1)Propsインターフェース（プロパティ・型）の定義
+interface Props{
+    id: number; 
+    name: string;
+    bounty: number;
+}
+
+//Emits型を定義
+type Emits = {
+    incrementBounty : [id: number];
+};
+
+//(2)definePropsを実行し、Propsオブジェクトの設定
+const props = defineProps<Props>();
+
+//Emitの設定
+const emit = defineEmits<Emits>();
+
+//値を監視し、変更が検知される状態にする
+// const bounty = ref(props.bounty);
+
+//「懸賞金アップ」ボタンをクリックしたときのメソッド
+const bountyUp = () : void =>{
+    emit("incrementBounty", props.id);
+}
+
+</script>
+
+<template>
+    <section class="box">
+        <p>{{ name }}</p>
+        <p>懸賞金 : {{ bounty }} ベリー</p>
+        <button @click="bountyUp">賞金アップ</button>
+    </section>
+</template>
+
+<style scoped>
+    .box{
+        border: black 5px solid;
+        margin: 3px;
+        padding: 10px;
+    }
+</style>
