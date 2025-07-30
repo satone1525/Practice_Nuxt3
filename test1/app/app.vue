bb4c1f740d0a231489130b1fd4bc6dace3ecca27
satone <kyotosato1107@gmail.com>
2025-07-30 22:50:08 +0900

    新規&学習: props

--- test1/app/app.vue
+++ test1/app/app.vue
@@ -1,13 +1,44 @@
-<script setup>
-  import Nami from '../components/Nami.vue';
-  import Luffy from '../components/monkey/Luffy.vue';
+<script setup lang="ts">
+  // import Nami from '../components/Nami.vue';
+  import OneCharacter from '../components/OneCharacter.vue';
+  // import Luffy from '../components/monkey/Luffy.vue';
+
+  //キャラクターリストデータ
+  const characterListInt = new Map<number, Character>();
+  characterListInt.set(1,{id:1, name: "ルフィ", bounty: 100000000});
+  characterListInt.set(2,{id:2, name: "ゾロ", bounty: 60000000});
+  const characterList = ref(characterListInt);
+
+  //懸賞金合計の算出プロパティ
+  const totalBounty = computed(
+    (): number => {
+      let total = 0;
+      for(const character of characterList.value.values()){
+        total += character.bounty;
+      }
+      return total;
+    }
+  )
+
+
+  //キャラクターインターフェース
+  interface Character{
+    id : number;
+    name : string;
+    bounty : number;
+  }
 </script>
 
 <template>
   <section>
-    <h1>キャラクター名</h1>
-    <Nami/>
-    <Luffy/>
+    <h1>キャラクターリスト</h1>
+    <p>懸賞金の合計: {{ totalBounty }}</p>
+    <OneCharacter
+      v-for="[id, character] in characterList"
+      :key = "id"
+      :name = "character.name"
+      :bounty="character.bounty"
+    />
   </section>
 </template>
 
---
+++ test1/components/OneCharacter.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+// 1. 子でpropsの準備
+//親から受け取るプロパティはdefineProps()で型宣言する
+//(1)Propsインターフェース（プロパティ・型）の定義
+interface Props{
+    name: string;
+    bounty: number
+}
+
+//(2)definePropsを実行し、Propsオブジェクトの設定
+const props = defineProps<Props>();
+
+//値を監視し、変更が検知される状態にする
+const bounty = ref(props.bounty);
+
+//「懸賞金アップ」ボタンをクリックしたときのメソッド
+const bountyUp = () : void =>{
+    bounty.value += 1000;
+}
+
+</script>
+
+<template>
+    <section class="box">
+        <p>{{ name }}</p>
+        <p>懸賞金 : {{ bounty }} ベリー</p>
+        <button @click="bountyUp">賞金アップ</button>
+    </section>
+</template>
+
+<style scoped>
+    .box{
+        border: black 5px solid;
+        margin: 3px;
+        padding: 10px;
+    }
+</style>
