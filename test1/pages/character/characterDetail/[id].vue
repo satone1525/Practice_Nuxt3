144f0a00c0b2e8d2e23fcab068c28d80ac2ecdcd
satone <kyotosato1107@gmail.com>
2025-08-21 01:00:51 +0900

    学習: useFetch関数 ★useAsync関数よりも簡潔に書けることを学んだよ。

--- test1/pages/character/characterDetail/[id].vue
+++ test1/pages/character/characterDetail/[id].vue
@@ -22,31 +22,49 @@ const route = useRoute();
 //キャラクターリストをステートから取得
 // const characterList = useState<Map<number, Character>>("characterList");
 
-const asyncData = await useAsyncData(
-    `/CharacterInfo/${route.params.id}`,    //キー文字（重複実行を避ける）
-    (): Promise<any> => {   //取得データの型
-        const characterInfoUrl = "/api/getOneCharacterInfo"     //アクセス先URLのクエリパラメータ以外を用意
-        //クエリパラメータの元データを用意  ★ここが動画と違う！！（エラー解決のために少し変更してある）
-        const params = {
-            id: route.params.id as string
-        };
-        const queryParams = new URLSearchParams(params);//クエリパラメータを生成
-        const urlFull = `${characterInfoUrl}?${queryParams}`    //URL作成
-        //URLに非同期でアクセスしてデータを取得
-        const response = $fetch(urlFull);
-        return response;    //取得データ
-
-        //メモ
-        // useAsyncData()の戻り値は4つある！！
-        // (1)data：取得したデータ
-        // (2)pending：データ取得が終了したかを表すbool値
-        // (3)refresh：データを再取得する関数
-        // (4)データ取得に失敗した際のエラーオブジェクト
+//クエリパラメータの元データを用意  ★ここが動画と違う！！（エラー解決のために少し変更してある）
+const params = {
+    id: route.params.id as string
+};
+//★useFetch関数を使うバージョン(useAsyncData関数よりも簡単)
+const asyncData = await useFetch(
+    "/api/getOneCharacterInfo",     //アクセス先URL：第一引数
+    {                               //第二引数
+        key:`/CharacterInfo/${route.params.id}`,    //キー文字（重複実行を避ける）
+        query:params
     }
 );
-
 //dataプロパティを取り出して代入
 const character = asyncData.data;
+
+
+
+// ★useAsyncData関数を使うバージョン
+// const asyncData = await useAsyncData(
+//     `/CharacterInfo/${route.params.id}`,    //キー文字（重複実行を避ける）
+//     (): Promise<any> => {   //取得データの型
+//         const characterInfoUrl = "/api/getOneCharacterInfo"     //アクセス先URLのクエリパラメータ以外を用意
+//         //クエリパラメータの元データを用意  ★ここが動画と違う！！（エラー解決のために少し変更してある）
+//         const params = {
+//             id: route.params.id as string
+//         };
+//         const queryParams = new URLSearchParams(params);//クエリパラメータを生成
+//         const urlFull = `${characterInfoUrl}?${queryParams}`    //URL作成
+//         //URLに非同期でアクセスしてデータを取得
+//         const response = $fetch(urlFull);
+//         return response;    //取得データ
+
+//         //メモ
+//         // useAsyncData()の戻り値は4つある！！
+//         // (1)data：取得したデータ
+//         // (2)pending：データ取得が終了したかを表すbool値
+//         // (3)refresh：データを再取得する関数
+//         // (4)データ取得に失敗した際のエラーオブジェクト
+//     }
+// );
+
+//dataプロパティを取り出して代入
+// const character = asyncData.data;
 
 </script>
 
@@ -61,17 +79,21 @@ const character = asyncData.data;
     <section>
         <h2>{{ PAGE_TITLE }}</h2>
         <table>
+            <!-- 
+             (1)character が 存在する場合 → character.id を表示する
+             (2)character が undefined や null の場合 → エラーにならずに undefined を返す
+             ※Vue のテンプレートでは undefined は空文字として表示されるので、画面上には何も出なくなります。 -->
             <tr>
                 <td>No：</td>
-                <td>{{ character.id}}</td>
+                <td>{{ character?.id}}</td>
             </tr>
             <tr>
                 <td>名前：</td>
-                <td>{{ character.name }}</td>
+                <td>{{ character?.name }}</td>
             </tr>
             <tr>
                 <td>懸賞金：</td>
-                <td>{{ character.bounty }}ベリー</td>
+                <td>{{ character?.bounty }}ベリー</td>
             </tr>
         </table>
     </section>
